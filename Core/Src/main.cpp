273373d415e7b00c1f97d47471501224747e2b13
sourland <jem2010@windowslive.com>
2023-10-04 09:58:05 +0300

    upload template

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -12,65 +12,39 @@
 #include "TimeKeepingTask.hpp"
 #include "TCHandlingTask.hpp"
 
-extern SPI_HandleTypeDef hspi1;
 extern UART_HandleTypeDef huart3;
-extern I2C_HandleTypeDef hi2c2;
-extern RTC_HandleTypeDef hrtc;
+
 
 template<class T>
 static void vClassTask(void *pvParameters) {
     (static_cast<T *>(pvParameters))->execute();
 }
 
-void blinkyTask1(void * pvParameters){
+void Task1(void * pvParameters){
     for(;;){
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-        HAL_Delay(50);
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
-        HAL_Delay(50);
+
     }
 }
 
-void blinkyTask2(void * pvParameters){
+void Task2(void * pvParameters){
     for(;;){
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-        HAL_Delay(300);
+
     }
 }
 
-namespace AT86RF215 {
-    AT86RF215 transceiver = AT86RF215(&hspi1, AT86RF215Configuration());
-}
+
 
 extern "C" void main_cpp(){
-    uartGatekeeperTask.emplace();
-    mcuTemperatureTask.emplace();
-    temperatureSensorsTask.emplace();
-    timeKeepingTask.emplace();
-    tcHandlingTask.emplace();
 
-    uartGatekeeperTask->createTask();
-    temperatureSensorsTask->createTask();
-    mcuTemperatureTask->createTask();
-    timeKeepingTask->createTask();
-    tcHandlingTask->createTask();
-
+    xTaskCreate(Task1, "blinkyTask 2", 1000, nullptr, tskIDLE_PRIORITY + 1, nullptr);
+    xTaskCreate(Task2, "blinkyTask 2", 1000, nullptr, tskIDLE_PRIORITY + 1, nullptr);
     vTaskStartScheduler();
 
     /**
      * Uncomment below and comment above for Led task visualization (for STM32H743)
      */
-//    xTaskCreate(blinkyTask1, "blinkyTask 2", 1000, nullptr, tskIDLE_PRIORITY + 1, nullptr);
-//    xTaskCreate(blinkyTask2, "blinkyTask 2", 1000, nullptr, tskIDLE_PRIORITY + 1, nullptr);
+
     for(;;);
     return;
 }
 
-/**
- * @brief This function handles EXTI line[15:10] interrupts.
- */
-extern "C" void EXTI15_10_IRQHandler(void) {
-    HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_14);
-
-    AT86RF215::transceiver.handle_irq();
-}
